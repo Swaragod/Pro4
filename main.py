78f9c48bac2d80b1a788d52b3285f2e16ad7d890
Vladimir Kovalev <wault@mail.ru>
2022-09-19 17:07:36 +0500

    переделал итератор

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
-print('new 1 - ')
-print()
-
-
 nested_list = [
-	['a', 'b', 'c'],
-	['d', 'e', 'f', 'h', False],
-	[1, 2, None, 11],
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None],
 ]
+
+
+print('\n', 'итератор', '\n')
+
 
 class SampleIterator:
 
-	def __init__(self,my_list):
-		self.my_list = my_list
+    def __init__(self, my_list):
+        self.my_list = my_list
 
-	def __iter__(self):
-		self.cur = -1
-		return self
+    def __iter__(self):
+        self.list_cur = 0
+        self.sub_list_cur = -1
 
-	def __next__(self):
-		self.cur += 1
-		if self.cur == len(self.my_list):
-			raise StopIteration
-		return self.my_list[self.cur]
+        return self
+
+    def __next__(self):
+        self.sub_list_cur += 1
+        self.sub_list = self.my_list[self.list_cur]
+        if self.sub_list_cur == len(self.sub_list):
+            self.sub_list_cur = 0
+            self.list_cur += 1
+            if self.list_cur == len(self.my_list):
+                raise StopIteration
+        return self.my_list[self.list_cur][self.sub_list_cur]
 
 
 my_iterator = SampleIterator(nested_list)
 for item in my_iterator:
-	my_iterator_low = SampleIterator(item)
-	for item_low in my_iterator_low:
-		print(item_low)
+    print(item)
 
-print('*')
-# print('old 1 - ')
-print()
+print('\n', 'комперхеншн', '\n')
 
-# def FlatIterator(list):
-# 	for sub_list in list:
-# 		for element in sub_list:
-# 			yield element
-#
-#for item in FlatIterator(nested_list):
-# 	print(item)
-
-
-flat_list = [item_low for item in my_iterator for item_low in item]
+flat_list = [item for item in SampleIterator(nested_list)]
 print(flat_list)
 
-print()
-print('2 - ')
-print()
+print('\n', 'генератор', '\n')
 
-numbers = [[print(i) for i in small_list] for small_list in nested_list]
+
+def my_generator(my_list):
+    for sub_list in my_list:
+        for element in sub_list:
+            yield element
+
+
+for item in my_generator(nested_list):
+    print(item)
